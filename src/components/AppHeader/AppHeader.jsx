b1e74a98bfeb783cc6872f29b7f15b2ab99e41aa
Fhomi Zuenda <zuenda25@gmail.com>
2022-05-22 14:24:33 +0700

    App Bar

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,9 +1,34 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import './AppHeader.css';
+import { FaBars, FaSearch, FaRegUserCircle } from 'react-icons/fa';
+import dataSource from '../../sources/data.source';
 
 function AppHeader() {
   return (
     <header>
-      Ini Header
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <button className="py-4 px-2" type="submit"><FaBars /></button>
+          <img className="app-logo" src="./images/fd-logo.png" alt="Female Daily Logo" />
+        </div>
+        <div className="search-bar">
+          <FaSearch />
+          <input className="search-input" type="text" placeholder="Search products, articles, topics, brands, etc" />
+        </div>
+        <button type="submit" className="btn-auth">
+          <FaRegUserCircle className="mr-2" />
+          {' '}
+          LOGIN / SIGNUP
+        </button>
+      </div>
+      <hr />
+      <div className="nav-header">
+        {dataSource.topNav.map((item) => (
+          <a href="#top">{item}</a>
+        ))}
+      </div>
+      <hr />
     </header>
   );
 }
