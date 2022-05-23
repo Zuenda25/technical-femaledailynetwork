d3fa1a5ab6972d53d62f51476c0ae7523474505f
Fhomi Zuenda <zuenda25@gmail.com>
2022-05-23 21:39:30 +0700

    add props show see more action

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,20 +1,24 @@
-/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './SectionTitle.css';
 import { FaAngleRight } from 'react-icons/fa';
 
 function TopBrands(props) {
+  const { title, subTitle, moreAction } = props;
   return (
     <div className="section-header">
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
       <div className="flex items-center justify-between">
-        <p className="subtitle">{props.subTitle}</p>
-        <a className="see-more" href="#top">
-          See more
-          {' '}
-          <FaAngleRight className="ml-2.5 mt-0.5" />
-        </a>
+        <p className="subtitle">{subTitle}</p>
+        {
+          moreAction && (
+          <a className="see-more" href="#top">
+            See more
+            {' '}
+            <FaAngleRight className="ml-2.5 mt-0.5" />
+          </a>
+          )
+        }
       </div>
     </div>
   );
