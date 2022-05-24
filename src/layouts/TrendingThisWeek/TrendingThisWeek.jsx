ff2b79e129d3514380701abc981fdbfb344c51e9
Fhomi Zuenda <zuenda25@gmail.com>
2022-05-24 14:20:52 +0700

    update some bug

--- src/layouts/TrendingThisWeek/TrendingThisWeek.jsx
+++ src/layouts/TrendingThisWeek/TrendingThisWeek.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import CardProduct from '../../components/CardProduct/CardProduct';
 
 import dataSource from '../../sources/data.source';
 
+const items = dataSource.trendingThisWeek.map((item) => (
+  <CardProduct product={item} />
+));
+
+const responsive = {
+  0: { items: 2 },
+  568: { items: 4 },
+  1024: { items: 6 },
+};
+
 function TrendingThisWeek() {
   return (
     <div className="section">
       <SectionTitle title="Trending This Week" subTitle="See our weekly most reviewed products" />
-      <div className="py-4 grid grid-cols-6 gap-x-6">
-        {
-        dataSource.trendingThisWeek.map((item) => (
-          <CardProduct product={item} />
-        ))
-      }
-      </div>
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        controlsStrategy="responsive"
+        infinite
+        disableButtonsControls
+      />
     </div>
   );
 }
